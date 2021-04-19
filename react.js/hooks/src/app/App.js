692493dee180dba03555069ed19b99486811dcd1
Elisei <=>
2021-04-19 21:02:09 +0600

    axios

--- react.js/hooks/src/app/App.js
+++ react.js/hooks/src/app/App.js
@@ -1,95 +1,26 @@
 import React, { useState } from 'react';
- 
-// const App = () => {
-//     // const[car,setCar]=useState ("BMV")
-//     const [count, setCount] = useState(0)
-//     const [color, setColor] = useState("black")
-
-
-//     return (
-//         <div>
-//             {/* <h1>{car}</h1>
-//             <button onClick={()=>{
-//                 setCar("MERS")
-//             // }}>MERS</button> */}
-
-
-//             {/* <h1>Counter</h1>
-//             <h2>{count}</h2>
-//             <button onClick={
-//                 ()=>{
-//                     setCount(count+1)
-//                 }
-//             }>+</button>
-//               <button onClick={
-//                 ()=>{
-//                     setCount(count0-1)
-//                 }
-//             }>-</button> */}
-
-
-//             <h1>Counter</h1>
-//             <h2 style={{color}}>{count}</h2>
-//             <button onClick={()=>setCount(count+1)}>+</button>
-//             <button onClick={()=>setCount(count-1)}>-</button>
-//             <button onClick={()=>setCount(count*2)}>x*2</button>
-//             <button onClick={()=>setCount(count/2)}>x/2</button>
-//             <button onClick={()=>setColor("green")}>green</button>
-//             <button onClick={()=>setColor("red")}>red</button>
-//             <button onClick={()=>setColor("blue")}>blue</button>
-//         </div>
-//     );
-// };
 
 const App = () => {
-    const [input, setInput] = useState("")
-   
-
-
+    const [top,setTop]=useState(0)
+    const[left,setLeft]=useState(0)
     return (
         <div>
-            <h1>Calculator</h1>
-            <h2>{input}</h2>
-            <table>
-                <tr>
-                    <td><button onClick={()=>setInput(input+"+")}>+</button></td>
-                    <td><button onClick={()=>setInput(input+"-")}>-</button></td>
-                    <td><button onClick={()=>setInput(input+"*")}>*</button></td>
-                    <td><button onClick={()=>setInput(input+"/")}>/</button></td>
-                </tr> 
-            </table>
-            <table>
-                <tr>
-                    <td><button onClick={()=>setInput(input+"1")}>1</button></td>
-                    <td><button onClick={()=>setInput(input+"2")}>2</button></td>
-                    <td><button onClick={()=>setInput(input+"3")}>3</button></td>
-                    <td><button onClick={()=>setInput(input+"4")}>4</button></td>
-                </tr>
-            </table>
-            <table>
-                <tr>
-                    <td><button onClick={()=>setInput(input+"5")}>5</button></td>
-                    <td><button onClick={()=>setInput(input+"6")}>6</button></td>
-                    <td><button onClick={()=>setInput(input+"7")}>7</button></td>
-                    <td><button onClick={()=>setInput(input+"8")}>8</button></td>
-                </tr>
-            </table>
-            <table>
-                <tr>
-                    <td><button onClick={()=>setInput(input+"9")}>9</button></td>
-                    <td><button onClick={()=>setInput(input+"0")}>0</button></td>
-                    <td><button onClick={()=>setInput(input+"00")}>00</button></td>
-                    <td><button onClick={()=>setInput(""+eval(input))}>=</button></td>
-                </tr>
-            </table>
-            <td><button onClick={()=>setInput(input +".")}>.</button></td>
-            <td><button onClick={()=>setInput("")}>C</button></td>
-<td>
-    <button onClick={()=> setInput(input.slice(0,input.length-1))}>
-        {"<"}
-    </button>
-</td>
-        
+            <div>
+                <button onClick={()=>setLeft(left-5)}>Left</button>
+                <button onClick={()=>setLeft(left+5)}>Right</button>
+                <button onClick={()=>setTop(top-5)}>Top</button>
+                <button onClick={()=>setTop(top+5)}>Down</button>
+            </div>
+            <div className style={{
+            background:"green",
+            width:"100px",
+            height:"100px",
+            marginTop:top+"px",
+            marginLeft:left+"px"
+        }}
+        className="cub">
+
+        </div>
         </div>
     );
 };
